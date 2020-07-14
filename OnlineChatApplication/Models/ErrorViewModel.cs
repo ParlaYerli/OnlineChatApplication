47fee66a5fa64b8a8e595985fa5c78aace1233f8
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-07-14 22:08:17 +0300

    Add project files.

---
+++ OnlineChatApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineChatApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
